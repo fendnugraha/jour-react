7f1aa3b9c3f521a77cda324a02fb0e2a317c8352
fendnugraha <fendnugraha92@gmail.com>
2024-12-12 19:43:14 +0700

    buat dashboard

--- src/components/PosPage.jsx
+++ src/components/PosPage.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import ProductList from "./ProductList";
 import Cart from "./Cart";
 import Header from "./Header";
-import { ShoppingBagIcon } from "@heroicons/react/16/solid";
+import {
+    ArrowRightCircleIcon,
+    CheckBadgeIcon,
+    PresentationChartBarIcon,
+    ShoppingBagIcon,
+    WrenchIcon,
+} from "@heroicons/react/16/solid";
 
 const PosPage = ({ cart, addToCart }) => {
     return (
@@ -10,30 +16,114 @@ const PosPage = ({ cart, addToCart }) => {
             {/* <h1 className="text-2xl font-bold mb-4">Point of Sales - Add to Cart</h1> */}
             <Header title={"Point of Sales - Add to Cart"} />
             <div className="grid grid-cols-4 grid-rows-2 gap-4 p-4 h-[80%]">
-                <div className="flex flex-col p-6 items-start justify-center gap-4 shadow-sm bg-white rounded-2xl">
+                <div className="flex flex-col p-6 items-start justify-between gap-4 shadow-sm bg-white rounded-2xl">
                     <div>
                         <h1 className="text-sm">
-                            <ShoppingBagIcon className="w-4 h-4 inline" /> Total Sales
+                            <PresentationChartBarIcon className="w-4 h-4 inline text-indigo-600" /> Total Sales
                         </h1>
                     </div>
                     <div>
-                        <h1 className="text-2xl font-bold">Rp. 200,000,000</h1>
+                        <span>Rp.</span>
+                        <h1 className="text-3xl font-bold">200,000,000</h1>
                     </div>
-                    <div className="border-t w-full p-4 flex justify-end">
-                        <a>View report</a>
+                    <div className="border-t w-full pt-2 flex justify-end">
+                        <a href="#" className="text-gray-600">
+                            View report <ArrowRightCircleIcon className="w-4 h-4 ml-2 align-middle inline" />
+                        </a>
                     </div>
                 </div>
-                <div className="flex justify-center items-center shadow-sm bg-white rounded-2xl">
-                    <h1 className="text-2xl">Grid 2</h1>
+                <div className="flex flex-col p-6 items-start justify-between gap-4 shadow-sm bg-white rounded-2xl">
+                    <div>
+                        <h1 className="text-sm">
+                            <ShoppingBagIcon className="w-4 h-4 inline text-indigo-600" /> Total Orders
+                        </h1>
+                    </div>
+                    <div>
+                        <span>Orders</span>
+                        <h1 className="text-3xl font-bold">200</h1>
+                    </div>
+                    <div className="border-t w-full pt-2 flex justify-end">
+                        <a href="#" className="text-gray-600">
+                            View report <ArrowRightCircleIcon className="w-4 h-4 ml-2 align-middle inline" />
+                        </a>
+                    </div>
                 </div>
-                <div className="flex justify-center items-center shadow-sm col-span-2 row-span-2 bg-white rounded-2xl">
-                    <h1 className="text-2xl">Grid 3</h1>
+                <div className="p-6 shadow-sm col-span-2 row-span-2 bg-white rounded-2xl">
+                    <h1 className="text-2xl font-bold">New order</h1>
+                    <table className="w-full mt-4 table-auto text-sm">
+                        <thead>
+                            <tr className="border-b">
+                                <th className="p-4">Order number</th>
+                                <th>Phone type</th>
+                                <th>Customer</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr className="border-b">
+                                <td className="p-3">Order number</td>
+                                <td>Phone type</td>
+                                <td>Customer</td>
+                                <td>Status</td>
+                            </tr>
+                            <tr className="border-b">
+                                <td className="p-3">Order number</td>
+                                <td>Phone type</td>
+                                <td>Customer</td>
+                                <td>Status</td>
+                            </tr>
+                            <tr className="border-b">
+                                <td className="p-3">Order number</td>
+                                <td>Phone type</td>
+                                <td>Customer</td>
+                                <td>Status</td>
+                            </tr>
+                            <tr className="border-b">
+                                <td className="p-3">Order number</td>
+                                <td>Phone type</td>
+                                <td>Customer</td>
+                                <td>Status</td>
+                            </tr>
+                            <tr className="border-b">
+                                <td className="p-3">Order number</td>
+                                <td>Phone type</td>
+                                <td>Customer</td>
+                                <td>Status</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-                <div className="flex justify-center items-center shadow-sm bg-white rounded-2xl">
-                    <h1 className="text-2xl">Grid 4</h1>
+                <div className="flex flex-col p-6 items-start justify-between gap-4 shadow-sm bg-white rounded-2xl">
+                    <div>
+                        <h1 className="text-sm">
+                            <WrenchIcon className="w-4 h-4 inline text-indigo-600" /> On Process
+                        </h1>
+                    </div>
+                    <div>
+                        <span>Unit</span>
+                        <h1 className="text-3xl font-bold">50</h1>
+                    </div>
+                    <div className="border-t w-full pt-2 flex justify-end">
+                        <a href="#" className="text-gray-600">
+                            View report <ArrowRightCircleIcon className="w-4 h-4 ml-2 align-middle inline" />
+                        </a>
+                    </div>
                 </div>
-                <div className="flex justify-center items-center shadow-sm bg-white rounded-2xl">
-                    <h1 className="text-2xl">Grid 5</h1>
+                <div className="flex flex-col p-6 items-start justify-between gap-4 shadow-sm bg-white rounded-2xl">
+                    <div>
+                        <h1 className="text-sm">
+                            <CheckBadgeIcon className="w-4 h-4 inline text-indigo-600" /> Finished
+                        </h1>
+                    </div>
+                    <div>
+                        <span>Unit</span>
+                        <h1 className="text-3xl font-bold">30</h1>
+                    </div>
+                    <div className="border-t w-full pt-2 flex justify-end">
+                        <a href="#" className="text-gray-600">
+                            View report <ArrowRightCircleIcon className="w-4 h-4 ml-2 align-middle inline" />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
